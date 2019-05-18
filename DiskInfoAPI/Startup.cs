3b8c5525c88c43051a07826a03b85b2e8afbc790
Brian Soetens <brian40@live.nl>
2019-05-18 20:29:10 +0200

    Removed https redirection and updated gitignore

--- DiskInfoAPI/Startup.cs
+++ DiskInfoAPI/Startup.cs
@@ -42,8 +42,7 @@ namespace DiskInfoAPI
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
-
-            app.UseHttpsRedirection();
+            
             app.UseMvc();
         }
     }
